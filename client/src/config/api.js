ffa42f7c8431da5ba2b0f0e04f403031a54f2738
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-12-02 16:13:57 -0600

    trying to get the local host gone

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,8 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
+console.log('NODE_ENV:', process.env.NODE_ENV);
 const API_URL = isDevelopment 
   ? 'http://localhost:5001/api'
   : 'https://esdoriginaltestingapp-production.up.railway.app/api';
+console.log('API_URL:', API_URL);
 
 export default API_URL; 
