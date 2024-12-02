a9f52643b8d492a652dbcf6b0bf56ef697f11dfa
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-12-02 15:57:10 -0600

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,7 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.PGUSER || 'postgres',
-    host: process.env.PGHOST || 'localhost',
-    database: process.env.PGDATABASE || 'esd_testing_db',
-    password: process.env.PGPASSWORD || 'postgres',
-    port: process.env.PGPORT || 5432,
-    ssl: false
+    connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
