4746a8e5fb8054f8dd8d0c17027fc327974b76db
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-12-03 12:56:01 -0600

    a;ldkjfiifbnel

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       }
