679b4d719d5c6f73455156ad8975aa128a5f9b8d
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-12-06 13:02:49 -0600

    Update Navbar.jsx
    Update Admin button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar({ isAdmin, isAuthenticated }) {
                                 to="/login"
                                 className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300"
                             >
-                                Admin Login
+                                Manager/Admin Login
                             </Link>
                         ) : (
                             <button
